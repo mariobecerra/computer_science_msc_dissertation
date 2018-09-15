de9f549028fe9499a52eb0d4c63526d6d714c3cf
MBC <mbecerracontreras@gmail.com>
2018-09-14 23:57:16 -0500

    Update code for examples

--- R/BBVI_logistic_regression.R
+++ R/BBVI_logistic_regression.R
@@ -32,12 +32,15 @@ ones = function(P){
   return(rep(1.0, P))
 }
 
-create_data = function(N, real_mu, seed = 0){
+create_data = function(N, real_theta, seed = 0){
   set.seed(seed)
-  P = length(real_mu)
-  X = mvrnorm(N, mu = rep(0, P), Sigma = diag(rep(1, P)))
-  y = rbinom(N, 1, sigmoid(dot(X, real_mu)))
-  return(list(X = X, y = y))
+  # P = length(real_theta)
+  # X = mvrnorm(N, mu = rep(0, P), Sigma = diag(rep(1, P)))
+  # y = rbinom(N, 1, sigmoid(dot(X, real_theta)))
+  x = rnorm(N)
+  pr = sigmoid(real_theta[1] + real_theta[2]*x)
+  y = rbinom(N, 1, pr)
+  return(list(X = cbind(ones(N), x), y = y))
 }
 
 elbo_grad_samples = function(samples, mu, sigma_sq, y, X, P, prior_sigma){
@@ -69,15 +72,18 @@ vi = function(X, y, prior_sigma = 10000, max_iter = 6000, S = 10, eta = 1.0, see
   set.seed(seed)
   N = dim(X)[1]
   P = dim(X)[2]
-  mu = rnorm(P)
+  #mu = rnorm(P)
+  mu = rep(0, P)
   G = matrix(zeros(2*P*2*P), ncol = 2*P)
-  log_sigma_sq = rnorm(P)
+  # log_sigma_sq = rnorm(P)
+  log_sigma_sq = rep(0, P)
   mus = matrix(zeros(max_iter*P), ncol = P)
   norm_delta_lambda = zeros(max_iter)
   ELBO = zeros(max_iter)
   
   cat("Begin optimization\n\n")
   for(t in 1:max_iter){
+    # print(t)
     mus[t,] = mu
     sigma_sq = exp(log_sigma_sq)
     samples = mvrnorm(S, mu, diag(sigma_sq))
@@ -90,7 +96,7 @@ vi = function(X, y, prior_sigma = 10000, max_iter = 6000, S = 10, eta = 1.0, see
     log_sigma_sq_new = log_sigma_sq + rho_t[(P+1):(2*P)] * grad_estimate[(P+1):(2*P)]
     norm_delta_lambda_t = norm(mu_new - mu)
     norm_delta_lambda[t] = norm_delta_lambda_t
-    stop_criterion = norm_delta_lambda_t/norm(mu)
+    stop_criterion = norm_delta_lambda_t
     if(t %% 100 == 1){
       cat("", "\n")
       cat("Iteration: ", t, "\n")
@@ -101,7 +107,7 @@ vi = function(X, y, prior_sigma = 10000, max_iter = 6000, S = 10, eta = 1.0, see
       cat("Estimate ELBO: ", ELBO[t], "\n")
       cat("Stop criterion value: ", stop_criterion, "\n")
     }
-    if(stop_criterion < 0.00003){
+    if(stop_criterion < 0.005){
       cat("Breaking\n\n")
       break
     }
@@ -127,11 +133,13 @@ vi = function(X, y, prior_sigma = 10000, max_iter = 6000, S = 10, eta = 1.0, see
 
 # Run VI ------------------------------------------------------------------
 
-real_mu = c(-10, 10)
+real_theta = c(-5, 5)
 
-dat = create_data(300, real_mu, seed = 0)
+dat = create_data(500, real_theta, seed = 0)
 
-mod = vi(dat$X, dat$y, S = 30, max_iter = max_iter, seed = 0)
+mod_glm <- glm(dat$y ~ dat$X[,2], family = "binomial")
+
+mod = vi(dat$X, dat$y, S = 50, max_iter = max_iter, seed = 0)
 
 
 # VI results --------------------------------------------------------------
@@ -143,7 +151,8 @@ colours = c('red', 'blue')
 
 post_dist_plot = tibble(x = c(mu + 4*sigma, mu - 4*sigma)) %>% 
   ggplot(aes(x)) + 
-  geom_vline(xintercept = real_mu, col = colours, linetype = "dotted") +
+  geom_vline(xintercept = real_theta, col = "grey", linetype = "dashed") +
+  geom_vline(xintercept = mod_glm$coefficients, col = colours, linetype = "dotted") +
   mapply(function(mean, sd, col) {
     stat_function(fun = dnorm, args = list(mean = mean, sd = sd), col = col)
   }, 
@@ -151,7 +160,7 @@ post_dist_plot = tibble(x = c(mu + 4*sigma, mu - 4*sigma)) %>%
   sd = sigma, 
   col = colours
   ) +
-  xlab(expression(beta)) +
+  xlab(expression(mu)) +
   ylab("p")
 
 elbo_iter_plot = tibble(ELBO = mod$ELBO) %>% 
@@ -164,23 +173,30 @@ mus_iteration_individual_plot = mod$mus %>%
   mutate(Iteration = 1:nrow(.)) %>% 
   gather(key, value, -Iteration) %>% 
   ggplot() +
-  geom_hline(yintercept = real_mu[1], linetype = "dotted", color = colours[1], size = 0.4) +
-  geom_hline(yintercept = real_mu[2], linetype = "dotted", color = colours[2], size = 0.4) +
+  # geom_hline(yintercept = real_theta[1], linetype = "dotted", color = colours[1], size = 0.4) +
+  # geom_hline(yintercept = real_theta[2], linetype = "dotted", color = colours[2], size = 0.4) +
+  geom_hline(yintercept = mod_glm$coefficients[1], linetype = "dotted", color = colours[1], size = 0.4) +
+  geom_hline(yintercept = mod_glm$coefficients[2], linetype = "dotted", color = colours[2], size = 0.4) +
   geom_line(aes(Iteration, value, colour = key), show.legend = F, size = 0.4) +
-  ylab(expression(beta)) +
+  ylab(expression(mu)) +
   scale_color_manual(values = colours) 
 
 mus_iteration_joint_plot = mod$mus %>% 
   as_tibble() %>% 
   ggplot() +
-  geom_hline(yintercept = 10, color = 'red', linetype = 'dotted') +
-  geom_vline(xintercept = -10, color = 'red', linetype = 'dotted') +
+  # geom_hline(yintercept = 10, color = 'red', linetype = 'dotted') +
+  # geom_vline(xintercept = -10, color = 'red', linetype = 'dotted') +
+  geom_vline(xintercept = mod_glm$coefficients[1], color = 'red', linetype = 'dotted') +
+  geom_hline(yintercept = mod_glm$coefficients[2], color = 'red', linetype = 'dotted') +
   geom_path(aes(V1, V2), size = 0.4, alpha = 0.9) +
-  geom_point(data = tibble(V1 = real_mu[1], V2 = real_mu[2]),
+  geom_point(data = tibble(V1 = mod_glm$coefficients[1], V2 = mod_glm$coefficients[2]),
              aes(V1, V2),
              colour = "red", size = 1) +
-  xlab(expression(beta[0])) +
-  ylab(expression(beta[1]))
+  # geom_point(data = tibble(V1 = real_theta[1], V2 = real_theta[2]),
+  #            aes(V1, V2),
+  #            colour = "red", size = 1) +
+  xlab(expression(mu[0])) +
+  ylab(expression(mu[1]))
 
 out_plot = arrangeGrob(elbo_iter_plot, post_dist_plot, 
                        mus_iteration_individual_plot, mus_iteration_joint_plot, 
@@ -192,14 +208,14 @@ ggsave(out_plot, filename = "BBVI_plots.pdf", dpi = 250, width = 12, height = 8,
 
 # Posterior predictive ----------------------------------------------------
 
-test_dat = create_data(20, real_mu, seed = 0)
-
-post_samples = mvrnorm(1000, mu = mod$mu, Sigma = diag(mod$sigma_sq))
-
-probs = sigmoid(post_samples %*% t(test_dat$X))
-
-mean_probs = colMeans(probs)
-
-preds = ifelse(mean_probs > 0.5, 1, 0)
-
-table(preds, test_dat$y)
+# test_dat = create_data(20, real_theta, seed = 0)
+# 
+# post_samples = mvrnorm(1000, mu = mod$mu, Sigma = diag(mod$sigma_sq))
+# 
+# probs = sigmoid(post_samples %*% t(test_dat$X))
+# 
+# mean_probs = colMeans(probs)
+# 
+# preds = ifelse(mean_probs > 0.5, 1, 0)
+# 
+# table(preds, test_dat$y)
--- R/gradient_descent_example.R
+++ R/gradient_descent_example.R
@@ -1,122 +1,7 @@
 library(tidyverse)
+library(gridExtra)
 
-theme_set(theme_bw(base_size = 20))
-
-system("mkdir ../../out/optim")
-
-###################################
-###################################
-### Linear Regression
-###################################
-###################################
-
-N <- 1000
-beta_0 <- 2
-beta_1 <- 1
-
-set.seed(124362)
-data <- tibble(x = rnorm(N),
-               y = beta_0 + beta_1*x + rnorm(N))
-
-modelo <- lm(y~x, data = data)
-
-
-(data %>% 
-  ggplot(aes(x, y)) +
-  geom_point(size = 0.7, alpha = 0.6) +
-  geom_abline(slope = beta_1, intercept = beta_0)
-) %>% 
-  ggsave(.,
-         file = "../../out/optim/lin_reg_example_1.pdf",
-         device = 'pdf')
-
-gradient <- function(data, betas){
-  n <- nrow(data)
-  const <- rep(1, nrow(data))
-  li <- data$y - betas[1]*const - betas[2]*data$x
-  g1 <- -2*sum(li)/n
-  g2 <- -2*sum(li*data$x)/n
-  return(c(g1, g2))
-}
-
-
-max_it <- 100
-data_gradient_descent <- tibble(it = 1:max_it,
-                                beta_0 = rep(0, max_it),
-                                beta_1 = rep(0, max_it),
-                                gradient_norm = rep(0, max_it))
-
-i = 0
-betas <- c(0, 0)
-alpha = 0.1
-while(i < max_it){
-  i = i + 1
-  g <- gradient(data, betas)
-  g_norm <- sqrt(sum(g^2))
-  g_unit <- g/g_norm
-  cat("It: ", i, 
-      "\n\tbeta_0: ", betas[1], 
-      "\n\tbeta_1: ", betas[2],
-      "\n\tgradient_norm: ", g_norm, 
-      "\n\tDirection: ", g_unit[1], g_unit[2],
-      "\n\n")
-  data_gradient_descent$beta_0[i] <- betas[1]
-  data_gradient_descent$beta_1[i] <- betas[2]
-  data_gradient_descent$gradient_norm[i] <- g_norm
-  if(g_norm < 0.000001) break
-  betas <- betas - alpha*g
-}
-
-
-data_gradient_descent <- data_gradient_descent[1:i,]
-
-data_gradient_descent %>% 
-  ggplot(aes(it, gradient_norm)) +
-  geom_point(size = 0.7) +
-  geom_line(size = 0.4)
-
-
-
-# data_gradient_descent %>% 
-#   ggplot(aes(beta_0, beta_1)) +
-#   geom_path(size = 0.3, color = 'red') +
-#   geom_point(size = 0.7, alpha = 0.4)
-
-
-(data_gradient_descent %>% 
-  ggplot(aes(beta_0, beta_1)) +
-  xlab("Beta 0") +
-  ylab("Beta 1") +
-  geom_segment(
-    aes(
-      xend = c(tail(beta_0, n = -1), NA),
-      yend = c(tail(beta_1, n = -1), NA)
-    ),
-    size = 0.4,
-    color = '#919191',
-    arrow = arrow(length = unit(0.18, "cm"))
-    ) +
-  #geom_path(size = 0.6, color = 'red') +
-  geom_point(size = 0.4, color = 'black') +
-  geom_point(aes(x, y),
-             data = tibble(x = beta_0,
-                           y = beta_1)) +
-  geom_point(aes(x, y),
-             data = tibble(x = modelo$coefficients[1],
-                           y = modelo$coefficients[2]),
-             shape = 'x',
-             size = 5) +
-  theme(
-    #panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()
-    )
-) %>% 
-  ggsave(.,
-         file = "../../out/optim/lin_reg_example_1_GD_iter.pdf",
-         device = 'pdf',
-         width = 8,
-         height = 5)
-
+theme_set(theme_bw(base_size = 12))
 
 ###################################
 ###################################
@@ -124,42 +9,27 @@ data_gradient_descent %>%
 ###################################
 ###################################
 
-rm(list = ls())
-
 N <- 1000
-beta_0 <- 1
-beta_1 <- 4
+theta_0 <- -5
+theta_1 <- 5
 
 set.seed(124362)
 data <- tibble(x = rnorm(N),
-               z = beta_0 + beta_1*x,
+               z = theta_0 + theta_1*x,
                pr = 1/(1 + exp(-z)),
                y = rbinom(1000, 1, pr))
 
-modelo <- glm(y ~ x, data = data, family = "binomial")
+mod_glm <- glm(y ~ x, data = data, family = "binomial")
 
-
-(data %>% 
-  ggplot(aes(x, y)) +
-  geom_point(size = 0.5, alpha = 0.4) +
-  stat_smooth(method="glm", 
-              method.args = list(family = "binomial"), 
-              se = FALSE,
-              size = 0.6,
-              color = 'black')
-) %>% 
-  ggsave(.,
-         file = "../../out/optim/log_reg_example_1.pdf",
-         device = 'pdf')
 
 h <- function(x){
   return(1/(1 + exp(-x)))
 }
 
-gradient <- function(data, betas){
+gradient <- function(data, thetas){
   n <- nrow(data)
   const <- rep(1, nrow(data))
-  bx <- betas[1]*const + betas[2]*data$x
+  bx <- thetas[1]*const + thetas[2]*data$x
   li <- data$y - h(bx)
   g1 <- -2*sum(li)/n
   g2 <- -2*sum(li*data$x)/n
@@ -167,10 +37,10 @@ gradient <- function(data, betas){
 }
 
 
-deviance <- function(betas, data = data){
+deviance <- function(thetas, data = data){
   n <- nrow(data)
   const <- rep(1, nrow(data))
-  bx <- betas[1]*const + betas[2]*data$x
+  bx <- thetas[1]*const + thetas[2]*data$x
   hbx <- h(bx)
   aux <- (1 - data$y)*log(1 - hbx)
   aux2 <- ifelse(is.nan(aux), 0, aux)
@@ -180,104 +50,80 @@ deviance <- function(betas, data = data){
 }
 
 
-backtrack <- function(betas, g, alpha, deviance, data, rho = 0.5, max_iter = 50, c1 = 10^-4){
-  funct_value <- do.call(deviance, list(betas, data))
-  norm_pk <- sum(g^2)
-  i = 0
-  while(i < max_iter){
-    i = i + 1
-    left <- do.call(deviance, list(betas - alpha*g, data))
-    right <- funct_value + c1*alpha*norm_pk
-    if(left <= right) break
-    alpha = alpha*rho
-  }
-  return(list(alpha = alpha,
-              iter = i))
-}
-
-
-
-max_it <- 1000
+max_it <- 10000
 data_gradient_descent <- tibble(it = 1:max_it,
-                                beta_0 = rep(0, max_it),
-                                beta_1 = rep(0, max_it),
+                                theta_0 = rep(0, max_it),
+                                theta_1 = rep(0, max_it),
                                 gradient_norm = rep(0, max_it),
                                 deviance = rep(0, max_it),
                                 alpha = rep(0, max_it))
 
 i = 0
-betas <- c(-10, 10) # reales 1 y 4
-g <- gradient(data, betas)
+thetas <- c(0, 0)
+g <- gradient(data, thetas)
 g_norm <- sqrt(sum(g^2))
 while(i < max_it){
   i = i + 1
   g_norm_0 <- g_norm # old gradient norm
-  g <- gradient(data, betas)
+  g <- gradient(data, thetas)
   g_norm <- sqrt(sum(g^2))
   g_unit <- g/g_norm
-  data_gradient_descent$beta_0[i] <- betas[1]
-  data_gradient_descent$beta_1[i] <- betas[2]
+  data_gradient_descent$theta_0[i] <- thetas[1]
+  data_gradient_descent$theta_1[i] <- thetas[2]
   data_gradient_descent$gradient_norm[i] <- g_norm
-  data_gradient_descent$deviance[i] <- deviance(betas, data)
-  if(g_norm < 0.0001 & g_norm/g_norm_0 > 0.8) break
-  backtrack_result <- backtrack(betas, g, 3, deviance, data)
-  alpha <- backtrack_result[[1]]
-  cat("It: ", i, 
-      "\n\tbeta_0: ", betas[1], 
-      "\n\tbeta_1: ", betas[2],
-      "\n\tgradient_norm: ", g_norm, 
-      "\n\tDirection: ", g_unit[1], g_unit[2],
-      "\n\talpha: ", alpha,
-      "\n\n")
+  data_gradient_descent$deviance[i] <- deviance(thetas, data)
+  if(g_norm < 0.0001) break
+  alpha = 2
+  if(i %% 100 == 1){
+    cat("It: ", i, 
+        "\n\ttheta_0: ", thetas[1], 
+        "\n\ttheta_1: ", thetas[2],
+        "\n\tgradient_norm: ", g_norm, 
+        "\n\tDirection: ", g_unit[1], g_unit[2],
+        "\n\talpha: ", alpha,
+        "\n\n")
+  }
   data_gradient_descent$alpha[i] <- alpha
-  betas <- betas - alpha*g
+  thetas <- thetas - alpha*g
 }
 
 
 data_gradient_descent <- data_gradient_descent[1:i,]
 
-data_gradient_descent %>% 
-  ggplot(aes(it, gradient_norm)) +
-  geom_point(size = 0.7) +
-  geom_line(size = 0.4)
-
-data_gradient_descent %>% 
+iter_loss_plot = data_gradient_descent %>% 
   ggplot(aes(it, deviance)) +
-  geom_point(size = 0.7) +
-  geom_line(size = 0.4)
+  geom_line(size = 0.5) +
+  xlab("Iteration") +
+  ylab("Loss function")
 
 
-
-(data_gradient_descent %>% 
-  #filter(it < 35) %>% 
-  ggplot(aes(beta_0, beta_1)) +
-  xlab("Beta 0") +
-  ylab("Beta 1") +
+iter_theta_plot =  data_gradient_descent %>% 
+  ggplot(aes(theta_0, theta_1)) +
+  geom_vline(xintercept = mod_glm$coefficients[1], color = 'red', linetype = 'dotted') +
+  geom_hline(yintercept = mod_glm$coefficients[2], color = 'red', linetype = 'dotted') +
+  xlab(expression(theta[0])) +
+  ylab(expression(theta[1])) +
   geom_segment(
     aes(
-      xend = c(tail(beta_0, n = -1), NA),
-      yend = c(tail(beta_1, n = -1), NA)
+      xend = c(tail(theta_0, n = -1), NA),
+      yend = c(tail(theta_1, n = -1), NA)
     ),
-    size = 0.4,
-    color = '#919191',
-    arrow = arrow(length = unit(0.18, "cm"))
+    size = 0.2,
+    color = '#919191'
   ) +
-  geom_point(size = 0.4, color = 'black') +
+  geom_point(size = 0.01, alpha = 0.6, color = 'black') +
   geom_point(aes(x, y),
-             data = tibble(x = beta_0,
-                           y = beta_1)) +
-  geom_point(aes(x, y),
-             data = tibble(x = modelo$coefficients[1],
-                           y = modelo$coefficients[2]),
-             shape = 'x',
-             size = 5) +
+             data = tibble(x = mod_glm$coefficients[1],
+                           y = mod_glm$coefficients[2]),
+             # shape = 'x',
+             color = 'red',
+             size = 1) +
   theme(panel.grid.minor = element_blank())
-) %>% 
-  ggsave(.,
-         file = "../../out/optim/log_reg_example_1_GD_iter.pdf",
-         device = 'pdf',
-         width = 8,
-         height = 5)
 
 
+out_plot = arrangeGrob(iter_loss_plot, iter_theta_plot, 
+                       ncol = 2)
 
+# plot(out_plot)
+
+ggsave(out_plot, filename = "GD_plots.pdf", dpi = 250, width = 15, height = 6, units = "cm")
