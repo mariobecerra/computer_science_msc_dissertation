ee1bfe8bac9f28e57beac60fa739c720eaa16326
Mario Humberto Becerra Contreras <mbecerra@EKT1369049MAC.local>
2018-09-29 19:12:37 -0500

    Compute approximate KL divergence of example

--- R/plot_KL_example.R
+++ R/plot_KL_example.R
@@ -53,3 +53,41 @@ plot_out = arrangeGrob(plot_1, plot_2, ncol = 2)
 
 plot(plot_out)
 
+
+
+
+library(FNN)
+
+KL.divergence(dat_plot$y1, dat_plot$y2, k=1)
+# 0.8769344
+
+KL.divergence(dat_plot$y2, dat_plot$y1, k=1)
+# 1.458181
+
+KL.divergence(dat_plot$y1, dat_plot$y3, k=1)
+# 2.168724
+
+KL.divergence(dat_plot$y3, dat_plot$y1, k=1)
+# 0.268002
+
+
+
+
+
+# KL.divergence(dat_plot$y1, dat_plot$y2, k=5)
+# # [1] 0.8769344 0.8782309 0.8784197 0.8787295 0.8789845
+# 
+# KL.divergence(dat_plot$y2, dat_plot$y1, k=5)
+# # [1] 1.458181 1.456432 1.456632 1.455998 1.455595
+# 
+# KL.divergence(dat_plot$y1, dat_plot$y3, k=5)
+# # [1] 2.168724 2.055499 2.044592 1.996628 1.970454
+# 
+# KL.divergence(dat_plot$y3, dat_plot$y1, k=5)
+# # [1] 0.2680020 0.2691186 0.2686312 0.2685130 0.2686499
+
+
+
+
+
+
